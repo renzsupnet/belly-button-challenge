d80765b51a4e7f68d7bb0f376582705f3d8a3964
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2025-04-23 17:52:37 -0700

    Added Bubble Chart using d3 data and Plotly

--- static/js/app.js
+++ static/js/app.js
@@ -5,12 +5,14 @@ function buildMetadata(sample) {
     // get the metadata field
     let metadata = data.metadata;
     console.log(metadata);
+
     // Filter the metadata for the object with the desired sample number
     let results = metadata.filter(data => data.id == sample);
     console.log(results);
 
     // Get the first result in the array since the results are an array of arrays
     let result = results[0];
+
     // Use d3 to select the panel with id of `#sample-metadata`
     let panel = d3.select("#sample-metadata");
 
@@ -20,7 +22,7 @@ function buildMetadata(sample) {
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
     Object.entries(result).forEach(([key, value]) => {
-      panel.append("h6").text(`${key}: ${value}`);
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
     })
   })
 }
@@ -29,20 +31,43 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-    // Get the samples field
+    console.log(data);
 
+    // Get the samples field
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let results = samples.filter(data => data.id == sample);
+    console.log(results);
+    let result = results[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
 
     // Build a Bubble Chart
+    let trace = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+      }
+    }
 
+    let layout = {
+      title: 'Bacteria Cultures Per Sample',
+      showLegend: false,
+      xaxis: { title: 'OTU ID' },
+      yaxis: { title: 'Number of Bacteria' },
+    }
 
     // Render the Bubble Chart
-
+    Plotly.newPlot('bubble', [trace], layout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
@@ -61,6 +86,7 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
+    buildCharts(data.names[0]);
     buildMetadata(data.names[0]);
 
     // Use d3 to select the dropdown with id of `#selDataset`
